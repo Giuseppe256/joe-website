cd6eb9fe6acd91509f4479e006fd7d242e747c69
Joe <massaro.jp256@gmail.com>
2020-08-07 00:35:02 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mt-0" id="app">
+    <div class="mx-0 px-0 pt-0">
+      <NavBar />
+    </div>
+    <router-view />
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <Home msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.pageHeader {
+  margin-top: 8px;
+}
+</style>
